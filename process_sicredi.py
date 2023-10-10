f023ebb34a453b527591345f95426b617b79fc1b
Eduardo Lanzarin <edulanzarin@outlook.com>
2023-10-10 00:17:22 -0300

    Adicionado a opção de escolher bancos e menu responsivo

--- process_sicredi.py
+++ process_sicredi.py
@@ -15,56 +15,52 @@ def paint_text_with_color(value):
 
 
 def process_sicredi(dados_pdf):
-    linhas_imprimir = True
-    linhas_impressas = 0
     data_list = []
     descricao_list = []
     valor_list = []
 
+    linhas_imprimir = True  # Variável de controle para a primeira página
+
     for pagina_num, pagina in enumerate(dados_pdf.pages, 1):
-        linhas_quebra = 0
+        texto_pagina = pagina.extract_text()
+        linhas = texto_pagina.split("\n")
 
-        if pagina_num == 1:
-            texto_pagina = pagina.extract_text()
-            linhas = texto_pagina.split("\n")
-
-            for linha in linhas:
+        for linha_num, linha in enumerate(linhas, 1):
+            partes = linha.split()
+            if len(partes) > 5:
+                if pagina_num == 1 and linha_num < 6:
+                    continue  # Ignorar as cinco primeiras linhas da primeira página
                 if "Saldo da conta" in linha:
                     linhas_imprimir = False
-                    break
+                    break  # Parar de processar quando encontrar "Saldo da conta"
 
-                if linhas_imprimir and linhas_quebra >= 6:
-                    partes = linha.split()
-                    if len(partes) > 5:
-                        data = partes[0]
-                        valor = partes[-2]
-                        descricao = " ".join(partes[1:-4])
+                data = partes[0]
+                valor = partes[-2]
+                descricao = " ".join(partes[1:-3])
 
-                        data_list.append(data)
-                        descricao_list.append(descricao)
-                        valor_list.append(valor)
+                data_list.append(data)
+                descricao_list.append(descricao)
+                valor_list.append(valor)
 
-                linhas_impressas += 1
-                linhas_quebra += 1
+        if not linhas_imprimir:
+            break  # Parar de processar páginas subsequentes
 
     # Criar um DataFrame com os dados
     df = pd.DataFrame(
-        {"DATA": data_list, "DESCRICAO": descricao_list, "VALOR": valor_list}
+        {
+            "DATA": data_list,
+            "DESCRICAO": descricao_list,
+            "NOTA": "",
+            "VALOR": valor_list,
+        }
     )
 
     # Substituir "." por "" e "," por "." em VALOR
-    df["VALOR"] = df["VALOR"].str.replace(".", "").str.replace(",", ".")
+    df["VALOR"] = (
+        df["VALOR"].str.replace(".", "").str.replace(",", ".").str.replace("-", "")
+    )
 
     # Aplicar estilos ao DataFrame
     df_styled = df.style.applymap(paint_text_with_color, subset=["VALOR"])
 
     return df_styled
-
-
-# Exemplo de uso
-pdf_file_path = "seu_arquivo.pdf"
-pdf_reader = PyPDF2.PdfFileReader(open(pdf_file_path, "rb"))
-
-styled_df = process_sicredi(pdf_reader)
-
-# Agora você pode usar styled_df em sua classe ou onde for necessário
